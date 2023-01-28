0a4411c128b8a2534b2570de75602ab9464ed15a
Thomas Fritsch <thomas@uidlt.fr>
2023-01-28 21:50:16 +0100

    correction indentation solution TP1

--- src/main.js
+++ src/main.js
@@ -19,17 +19,17 @@ const data = [
 	},
 ];
 function renderGameThumbnail({name, background_image, released, metacritic}){
-		const releasedDate = new Date(released);
-		return `<a href="${background_image}">
-				<img src="${background_image}" />
-				<footer>
-					<h3>${name}</h3>
-					<div class="infos">
-						<time datetime="${released}">${releasedDate.toLocaleDateString()}</time>
-						<span class="metacritic">${metacritic}</span>
-					</div>
-				</footer>
-			</a>`;
+	const releasedDate = new Date(released);
+	return `<a href="${background_image}">
+			<img src="${background_image}" />
+			<footer>
+				<h3>${name}</h3>
+				<div class="infos">
+					<time datetime="${released}">${releasedDate.toLocaleDateString()}</time>
+					<span class="metacritic">${metacritic}</span>
+				</div>
+			</footer>
+		</a>`;
 }
 let html = '';
 data.forEach( game  => html += renderGameThumbnail(game) );
