630d3f37e9f9482fb3be79e4e43659fe8dce4ccc
Thomas Fritsch <thomas@uidlt.fr>
2023-01-30 23:30:25 +0100

    maj solution TP2
    simplification en vue de l'exercice sur les modules

--- src/main.js
+++ src/main.js
@@ -18,68 +18,28 @@ const data = [
 		background_image: 'images/the-last-of-us-part-2.jpg',
 	},
 ];
-function renderGameThumbnail({ name, background_image, released, metacritic }) {
-	const releasedDate = new Date(released);
-	return `<a href="${background_image}">
-				<img src="${background_image}" />
-				<footer>
-					<h3>${name}</h3>
-					<div class="infos">
-						<time datetime="${released}">${releasedDate.toLocaleDateString()}</time>
-						<span class="metacritic">${metacritic}</span>
-					</div>
-				</footer>
-			</a>`;
-}
-/* Rendu de la liste des vignette remplacé par la partie TP2 / E. Formulaire de recherche */
-/* cf. fin de ce fichier */
-// let html = '';
-// data.forEach( game  => html += renderGameThumbnail(game) );
-// document.querySelector('.gameList').innerHTML = html;
 
 /* TP2 / B.2. Modifier des éléments */
+// Activation du lien du menu
+// const gameListLink = document.querySelector('.mainMenu .gameListLink');
+// gameListLink.setAttribute(
+// 	'class',
+// 	gameListLink.getAttribute('class') + ' active'
+// );
+// ou
+document.querySelector('.mainMenu .gameListLink').classList.add('active');
+// Affichage du titre h1
 document.querySelector('.viewTitle').innerHTML = '<h1>MAGASIN</h1>';
+// Modification du footer
 document.querySelector('body > footer > div:nth-of-type(2)').innerHTML +=
 	' / CSS inspirée de <a href="https://store.steampowered.com/">steam</a>';
 // ou
 // document.querySelectorAll('body > footer > div')[1].innerHTML +=
 // 	' / CSS inspirée de <a href="https://store.steampowered.com/">steam</a>';
 
-const gameListLink = document.querySelector('.mainMenu .gameListLink');
-gameListLink.setAttribute(
-	'class',
-	gameListLink.getAttribute('class') + ' active'
-);
-
-/* TP2 / C.2. Afficher/ masquer un élément */
-const toggleSearchButton = document.querySelector(
-	'.gameList .toggleSearchButton'
-);
-// comme on aura besoin plusieurs fois de ce querySelector (cf. fonction toggleSearchForm)
-// on le stocke dans une constante. C'est plus léger au niveau du code
-// et c'est aussi plus performant (on ne scanne le document qu'une seule fois)
-const searchForm = document.querySelector('.gameList .searchForm');
-/**
- * Fonction qui permet d'afficher ou masquer le formulaire de recherche
- * @param {Event} event événement déclenché par le bouton sur lequel on a cliqué
- */
-function toggleSearchForm(event) {
-	console.log('on a cliqué sur le bouton de recherche');
-	const isOpened = searchForm.getAttribute('style') !== 'display: none;';
-	if (!isOpened) {
-		searchForm.setAttribute('style', '');
-		toggleSearchButton.classList.add('opened');
-		searchInput.focus();
-	} else {
-		searchForm.setAttribute('style', 'display: none;');
-		toggleSearchButton.classList.remove('opened');
-	}
-}
-// Au clic sur le bouton "loupe" on appelle la fonction toggleSearchForm
-// NB: on passe une "référence" vers la fonction (son nom) pour qu'elle
-// puisse être appelée par le bouton quand on cliquera dessus,
-// on ne l'exécute pas nous même c'est à dire qu'on ne met pas `toggleSearchForm()`
-toggleSearchButton.addEventListener('click', toggleSearchForm);
+/* TP2 / C.3.4. Afficher la bonne page */
+// On affiche la gameList par défaut
+document.querySelector('.gameList').classList.add('active');
 
 /* TP2 / C.3. Navigation en JS : le menu */
 /**
@@ -115,9 +75,92 @@ function handleMenuLinkClick(event) {
 const menuLinks = document.querySelectorAll('.mainMenu a');
 menuLinks.forEach(link => link.addEventListener('click', handleMenuLinkClick));
 
-/* C.3.4. Afficher la bonne page */
-// on affiche la gameList par défaut
-document.querySelector('.gameList').classList.add('active');
+/* TP2 / C.2. Afficher/masquer un élément */
+const toggleSearchButton = document.querySelector(
+	'.gameList .toggleSearchButton'
+);
+const searchForm = document.querySelector('.gameList .searchForm');
+// comme on aura besoin plusieurs fois de ce querySelector searchForm
+// (cf. fonction toggleSearchForm) on le stocke dans une constante.
+// C'est plus léger au niveau du code et c'est aussi plus performant
+// (on ne scanne le document qu'une seule fois)
+/**
+ * Fonction qui permet d'afficher ou masquer le formulaire de recherche
+ * @param {Event} event événement déclenché par le bouton sur lequel on a cliqué
+ */
+function toggleSearchForm(event) {
+	console.log('on a cliqué sur le bouton de recherche');
+	const isOpened = searchForm.getAttribute('style') !== 'display: none;';
+	if (!isOpened) {
+		searchForm.setAttribute('style', '');
+		toggleSearchButton.classList.add('opened');
+	} else {
+		searchForm.setAttribute('style', 'display: none;');
+		toggleSearchButton.classList.remove('opened');
+	}
+}
+// Au clic sur le bouton "loupe" on appelle la fonction toggleSearchForm
+// NB: on passe une "référence" vers la fonction (son nom) pour qu'elle
+// puisse être appelée par le bouton quand on cliquera dessus,
+// on ne l'exécute pas nous même c'est à dire qu'on ne met pas `toggleSearchForm()`
+toggleSearchButton.addEventListener('click', toggleSearchForm);
+
+/* TP2 / E. Formulaire de recherche */
+// Soumission du formulaire de recherche
+function handleSearchFormSubmit(event) {
+	event.preventDefault();
+	const searchInput = searchForm.querySelector('[name=search]'),
+		orderingSelect = searchForm.querySelector('[name=ordering]');
+	renderGameList(searchInput.value, orderingSelect.selectedOptions[0].value);
+}
+searchForm.addEventListener('submit', handleSearchFormSubmit);
+
+function renderGameThumbnail({ name, background_image, released, metacritic }) {
+	const releasedDate = new Date(released);
+	return `<a href="${background_image}">
+				<img src="${background_image}" />
+				<footer>
+					<h3>${name}</h3>
+					<div class="infos">
+					<time datetime="${released}">${releasedDate.toLocaleDateString()}</time>
+					<span class="metacritic">${metacritic}</span>
+					</div>
+				</footer>
+			</a>`;
+}
+
+/**
+ * Fonction d'affichage de la liste des jeux.
+ * Utilisée :
+ * - au chargement de la page pour l'affichage initial
+ * - et à chaque fois qu'on soumet le formulaire de recherche
+ * @param {string} search chaîne de caractère recherchée dans le nom des jeux
+ * @param {string} ordering ordre d'affichage des résultats
+ */
+function renderGameList(search = '', ordering) {
+	// calcul de la fonction de tri selon le paramètre ordering
+	let sortingFunction;
+	switch (ordering) {
+		case '-metacritic':
+			sortingFunction = (a, b) => b.metacritic - a.metacritic;
+			break;
+		case '-released':
+			sortingFunction = (a, b) => b.released.localeCompare(a.released);
+			break;
+	}
+	// parcours du tableau + génération du code HTML de la gameList
+	let html = '';
+	data
+		.slice() // on clone le tableau pour que le sort ne modifie pas l'ordre des éléments
+		.filter(game => game.name.toLowerCase().includes(search.toLowerCase())) // recherche
+		.sort(sortingFunction) // tri
+		.forEach(name => (html += renderGameThumbnail(name))); // génération du HTML
+	// maj de la page HTML
+	document.querySelector('.gameList .results').innerHTML = html;
+}
+
+// rendu initial de la liste des jeux
+renderGameList();
 
 /* TP2 / D.2. Le formulaire de contact */
 /**
@@ -152,55 +195,12 @@ function handleHelpFormSubmit(event) {
 	bodyTextarea.value = '';
 	// on aurait aussi pu faire : form.reset();
 }
-// on écoute la soumission du formulaire
+// on écoute la soumission du formulaire de contact
 const helpForm = document.querySelector('.helpForm');
 helpForm.addEventListener('submit', handleHelpFormSubmit);
 
-/* TP2 / E. Formulaire de recherche */
-const sortByMetacritic = (a, b) => b.metacritic - a.metacritic;
-const sortByReleased = (a, b) => b.released.localeCompare(a.released);
-/**
- * Fonction d'affichage de la liste des jeux.
- * Utilisée pour l'affichage initial et à chaque fois qu'on utilise
- * le formulaire de recherche.
- * @param {string} search chaîne de caractère recherchée dans le nom des jeux
- * @param {string} ordering ordre d'affichage des résultats
- */
-function renderGameList(search = '', ordering) {
-	let html = '';
-	// calcul de la fonction de tri selon le paramètre ordering
-	let sortingFunction;
-	switch (ordering) {
-		case '-metacritic':
-			sortingFunction = sortByMetacritic;
-			break;
-		case '-released':
-			sortingFunction = sortByReleased;
-			break;
-	}
-	// parcours du tableau + génération du code HTML de la gameList
-	data
-		.slice() // on clone le tableau pour que le sort ne modifie pas l'ordre des éléments
-		.filter(game => game.name.toLowerCase().includes(search.toLowerCase())) // recherche
-		.sort(sortingFunction) // tri
-		.forEach(name => (html += renderGameThumbnail(name))); // génération du HTML
-	// maj de la page HTML
-	document.querySelector('.gameList .results').innerHTML = html;
-}
-// Soumission du formulaire de recherche
-const searchInput = searchForm.querySelector('[name=search]'),
-	orderingSelect = searchForm.querySelector('[name=ordering]');
-function handleSearchFormSubmit(event) {
-	event.preventDefault();
-	renderGameList(searchInput.value, orderingSelect.selectedOptions[0].value);
-}
-searchForm.addEventListener('submit', handleSearchFormSubmit);
-
-// rendu initial de la liste des jeux
-renderGameList();
-
 /*
-Solution des exercices précédents plus utiles pour le fonctionnement de l'app :
+Solution des exercices précédents désormais plus utiles pour le fonctionnement de l'app :
 */
 
 /* TP2 / B.1. Sélectionner des éléments */
